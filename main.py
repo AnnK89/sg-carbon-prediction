28ca5246373c0286bfbea3f60501f8ba6b2a5e84
siying png <siying.png@hotmail.com>
2023-09-24 23:13:01 +0800

    Updated setup files

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    # Retrieve data from BigQuery
+
+    # Load the model
+
+    # Make predictions
+
+    # Return the results
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ params.py
@@ -0,0 +1,10 @@
+##################  VARIABLES  ##################
+
+# Electricity : Input value (in KWh/Yr) X 0.85 (Emission Factor) =  Output value in (Kg of CO2)
+ELEC_EMISSION_FACTOR = 0.85
+
+# Petrol: Input Value(In Litres/Yr) X 2.296(Emission Factor) =  Output value in (Kg of CO2)
+# Diesel: Input Value(In Litres/Yr) X 2.653 (Emission Factor) =  Output value in (Kg of CO2)
+# LPG: Input Value(In Kg/Yr) X 2.983 (Emission Factor) =  Output value in (Kg of CO2)
+
+##################  CONSTANTS  #####################
