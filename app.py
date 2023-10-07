a66087491f2ed8babd373e459685bdf3a404d146
AnnKoh <annkoh12@gmail.com>
2023-10-07 10:29:15 +0800

    WIP

--- app.py
+++ app.py
@@ -10,12 +10,15 @@ data = requests.get("https://carbonpredictionarbon-nmh4nih7qa-as.a.run.app/predi
 data = pd.DataFrame(json.loads(data.json()['5_years_prediction']))
 
 df = BigQueryDataLoader.clean_pred_data(data)
+df['plan_area'] = df['plan_area'].apply(lambda x: x.upper())
+
 areas_map = json.load(open("MasterPlan2019PlanningAreaBoundaryNoSea.geojson","r"))
 
 fig=px.choropleth(df,
                   geojson=areas_map,
                   featureidkey='properties.Name',
                   locations='plan_area',
+                  animation_frame = 'year',
                   color='carbon_total',
                   color_continuous_scale='Inferno'
                   )
@@ -23,3 +26,6 @@ fig.update_geos(fitbounds="locations", visible=False)
 fig.update_layout(height=500, width=800)
 
 st.plotly_chart(fig, use_container_width=False, sharing="streamlit", theme="streamlit")
+
+
+
